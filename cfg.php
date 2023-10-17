409d6f86c2cdaa9a443b3e89b910dee7d6d9be32
Yeahman520 <rihards.barda@marupe.edu.lv>
2023-10-17 16:12:19 +0300

    neiet gan registration gan login

--- cfg.php
+++ cfg.php
@@ -3,14 +3,5 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $db = "proj2023";
-
-// Create connection
-$mysqli = new mysqli($servername, $username, $password, $db);
-
-// Check connection
-if($mysqli -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
-}
-echo"viss ok";
+$mysqli = new mysqli($servername,$username,$password,$db);
 ?>
--- proc.php
+++ proc.php
@@ -1,13 +1,13 @@
 <?php
 $l = $_POST["login"];
-$p = $a = $_POST["password"];
+$p = $_POST["password"];
 
 include('cfg.php');
-$result = mysqli_query("SELECT * FROM users WHERE (login='$1') AND (password='$p')");
+$result = $mysqli->query("SELECT * FROM users WHERE login='$l' AND password='$p'");
 if (mysqli_num_rows($result) > 0){
-    echo "GOOD!"
+    echo "GOOD!";
 }
 else{
-    echo "BYE BYE!"
+    echo "BYE BYE!";
 }
 ?>
